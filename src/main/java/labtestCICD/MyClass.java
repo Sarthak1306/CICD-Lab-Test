9765514ab4d62ff6857208d316cbd7162f4ee55c
Sarthak1306 <sarthak13062001@gmail.com>
2021-10-25 15:20:47 +0530

    Added Maven Project

---
+++ src/main/java/labtestCICD/MyClass.java
@@ -0,0 +1,15 @@
+package labtestCICD;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("CICD LAB Test 25/10/21");
+		System.out.println("Sarthak Srivastava");
+		int a = 10, b = 20;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
+}
